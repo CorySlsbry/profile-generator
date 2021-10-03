3d2753b4d794af63b2ed7bf40960242890b276c6
CorySlsbry <87287656+CorySlsbry@users.noreply.github.com>
2021-10-02 20:50:27 -0500

    employee manager engineer objects created

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+const { TestWatcher } = require('@jest/core');
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Cory');
+    
+    expect(employee.name).toBe('Cory');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    
+});
